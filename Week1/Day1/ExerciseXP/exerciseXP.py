18caad09e9f6915f0187493d676ce5d7b8a26405
Olga Sazanova <olga.korotkaya.ru@gmail.com>
2024-06-09 15:46:23 +0300

    Update exerciseXP.py

--- Week1/Day1/ExerciseXP/exerciseXP.py
+++ Week1/Day1/ExerciseXP/exerciseXP.py
@@ -1,1 +1,8 @@
-print('Hello World')
+#for _ in range(5):
+ #    print('Hello World')
+
+
+# Write code that calculates the result of: (99^3)*8 (meaning 99 to the power of 3, times 8).
+
+result = (99^3) *8
+print(result)
