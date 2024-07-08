48191fe632e90fbd742cae77f94ec86c4f357410
DiGureev <dianagureev@gmail.com>
2024-07-08 14:45:39 +0300

    feedback

---
+++ Hackathon #1 Facebook Radar/feedback.py
@@ -0,0 +1,13 @@
+# Project is working, made with tkinter. Code is clean and works fine. Connection to DB clear and correct. 
+# You also practiced modules, classes which is very good. Since in your Report class some methods don't require any self, you could use @staticmethod decorator. 
+# I think this import might also be a bit confusing:
+
+# import tkinter as tk
+# from tkinter import messagebox
+
+# If your code will be long enough it might looks like the messagebox you used is some kind of module itself and not a part of tk. 
+
+# Python OOP: 9/10
+# Modules: 9/10
+# Database:10/10
+# Overall: 8
